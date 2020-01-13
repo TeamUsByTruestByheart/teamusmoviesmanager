0ebbd5c4eebd22c11d1f23f863fb4c5fe293c11d
daniel mwangila <margalight@outlook.com>
2020-01-13 10:32:00 +0200

    creating the image-slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SearchComponent } from './components/search/search.component';
 import { BrowseComponent } from './components/browse/browse.component';
 import { ContactMeComponent } from './components/contact-me/contact-me.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { ImageSliderComponent } from './app/home/image-slider/image-slider.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.co
     SearchComponent,
     BrowseComponent,
     ContactMeComponent,
-    PagenotfoundComponent
+    PagenotfoundComponent,
+    ImageSliderComponent
   ],
   imports: [
     BrowserModule,
